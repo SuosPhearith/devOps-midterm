b73f831abc6e31300a68474dc6c4779b67e8dfec
SuosPhearith <suosphearith@gmail.com>
2024-03-29 11:07:19 +0700

    first commit

---
+++ src/test/java/com/midterm/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midterm.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
